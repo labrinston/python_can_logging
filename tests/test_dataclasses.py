7c23fabc9e463794ede0963740a0cfe6929f3c24
Liam Brinston <labrinston@gmail.com>
2025-11-28 19:07:02 -0800

    test(can2pwm): Test de/serialization +/- statusB

--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -10,5 +10,38 @@ class TestDataclasses(unittest.TestCase):
         # self.assertEqual(0, 1)
         self.assertTrue(True)
 
+    def test_statusB_Positive(self):
+        """Tests statusB packet de/encoding with positive current value"""
+
+        test_current = 1500
+        test_voltage = 5000
+        
+        original = icd.statusBPacket(current=test_current, voltage=test_voltage)
+        encoded = original.to_can_bytes()
+        decoded = icd.statusBPacket.from_can_bytes(encoded)
+
+        self.assertEqual(
+            decoded.current,
+            test_current
+        )
+
+        self.assertEqual(decoded.voltage, test_voltage)
+
+    def test_statusB_Negative(self):
+        """Tests statusB packet de/encoding with negative current value"""
+
+        test_current = -1500
+        test_voltage = 5000
+        
+        original = icd.statusBPacket(current=test_current, voltage=test_voltage)
+        encoded = original.to_can_bytes()
+        decoded = icd.statusBPacket.from_can_bytes(encoded)
+
+        self.assertEqual(decoded.current,
+                         test_current,
+                         f"""Current mismatch: expected {test_current}mA = 0x{test_current:04X},
+                         got: {decoded.current} = 0x{decoded.current:04X}"""
+                         )
+        self.assertEqual(decoded.voltage, test_voltage)
 if __name__ == '__main__':
     unittest.main()
