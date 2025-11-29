75f9620668c3ae34beed14adedcfecec08dbc4b2
Liam Brinston <labrinston@gmail.com>
2025-11-28 18:43:07 -0800

    feat(can2pwm): Add enums

--- Currawong/can2pwm_icd.py
+++ Currawong/can2pwm_icd.py
@@ -3,6 +3,7 @@
 from can import Message
 import dataclasses
 from dataclasses import dataclass
+from enum import Enum
 
 # Currawong Message format:
 #
@@ -13,3 +14,28 @@ from dataclasses import dataclass
 # +---------+--------------+-------------+----------------+
 
 class can2pwm():
+
+    DEVICE_TYPE = 0x0A 
+    # --- Enums --- #
+
+    class deviceType(Enum):
+        SERVO = 0x00
+        CAN2PWM = 0x0A
+
+    class inputModes(Enum):
+        STANDBY = 0
+        PWM = 1
+        RPM = 2
+
+    class feedbackModes(Enum):
+        NONE = 0
+        RPM = 1
+        PULSE_WIDTH = 2
+        DUTY_CYCLE = 3
+        ANALOG = 4
+
+    class messageTypes(Enum):
+        MULTI_COMMAND = 0x00
+        PWM_COMMAND = 0x10
+        HOME_COMMAND = 0x15
+        DISABLE_PACKET = 0x20
