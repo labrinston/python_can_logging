bcb7eb64343d306fc44b9092249758a272120f4e
Liam Brinston <labrinston@gmail.com>
2025-11-28 19:10:12 -0800

    teat(can2pwm): Test de/serialization serialNumber

--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -43,5 +43,39 @@ class TestDataclasses(unittest.TestCase):
                          got: {decoded.current} = 0x{decoded.current:04X}"""
                          )
         self.assertEqual(decoded.voltage, test_voltage)
+
+    def test_serialNumber(self):
+
+        # Test values
+        test_hw = 0x01
+        test_serial = 0x000174 # 371 in decimal
+        test_IDA = 0x02
+        test_IDB = 0xFF
+
+        # Values under test
+        original = icd.serialNumberPacket(
+            hwRev=test_hw,
+            serialNumber=test_serial,
+            userIDA=test_IDA,
+            userIDB=test_IDB)
+        encoded  = original.to_can_bytes()
+        decoded  = icd.serialNumberPacket.from_can_bytes(encoded)
+
+        # Expectations
+        expected_encoded = bytearray([0x01, 0x00, 0x01, 0x74, 0x00, 0x02, 0x00, 0xFF])
+
+        # Test the encode
+        self.assertEqual(encoded, expected_encoded,
+                         f"\nExpected: {expected_encoded}\nGot: {encoded}")
+
+        # Test the decode
+        self.assertEqual(decoded.serialNumber, test_serial,
+                         f"\nExpected: {test_serial:04X}\nGot: {decoded.serialNumber:04X}")
+        
+        self.assertEqual(decoded.userIDA, test_IDA,
+                         f"\nExpected: {test_IDA:04X}\nGot: {decoded.userIDA:04X}")
+        
+        self.assertEqual(decoded.userIDB, test_IDB,                        
+                         f"\nExpected: {test_IDB:04X}\nGot: {decoded.userIDB:04X}")
 if __name__ == '__main__':
     unittest.main()
