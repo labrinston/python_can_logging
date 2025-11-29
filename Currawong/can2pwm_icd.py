cf33e1f465e01d44e5fe33b3ae265aa287ed4311
Liam Brinston <labrinston@gmail.com>
2025-11-28 18:47:16 -0800

    feat(can2pwm): Add Packet base class
    Provides minimum set of expected methods.

--- Currawong/can2pwm_icd.py
+++ Currawong/can2pwm_icd.py
@@ -39,3 +39,57 @@ class can2pwm():
         PWM_COMMAND = 0x10
         HOME_COMMAND = 0x15
         DISABLE_PACKET = 0x20
+    @dataclass
+    class PacketBase:
+        """Base class for Currawong CANbus data packets"""
+
+        MESSAGE_TYPE = None
+        
+        def to_bytes(self) -> bytearray:
+            """Converts to wire format from dataclass fields."""
+
+        @classmethod
+        def from_bytes(cls, data: bytes):
+            """Converts to dataclass fields from wire format."""
+        
+        # Does it make sense to do this here?
+        @classmethod
+        def csv_header(cls, fields=None):
+            print(f"Creating csv headers for: {fields}")
+            if fields is None:
+                return [f.name for f in dataclasses.fields(cls)]
+            else:
+                # Should probably validate the fields so we can fail at header construction instead of on data retrival
+                return fields
+
+        def to_csv(self, fields=None):
+            if fields is None:
+                # This should convert things to a string
+                # for f in dataclasses.fields(self): 
+                return [f"{getattr(self, f.name)}" for f in dataclasses.fields(self)]
+            else:
+                return [f"{getattr(self, field)}" for field in fields]
+
+        # def to_csv_row(self, include_timestamp=True):
+
+        #     if self.LOG_FIELDS is None:
+        #         fields = [v for k, v in self.__dict__.items() if not k.startswith('_')]
+        #     else:
+        #         fields = [getattr(self, field) for field in self.LOG_FIELDS]
+        #     return fields
+
+        # @classmethod
+        # def csv_header(cls):
+        #     """Return CSV header as a list"""
+
+        #     if cls.LOG_FIELDS is None:
+        #         # Get all fields            
+        #         fields = [ f.name for f in dataclasses.fields(cls) ]
+        #     else:
+        #         fields = cls.LOG_FIELDS
+
+        #     return fields
+
+        # If we want to return a string instead?
+        # def to_csv_string(self):
+        
