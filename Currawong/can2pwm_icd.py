66c3522a6329945ec2e1f43e806693715bee6dd4
Liam Brinston <labrinston@gmail.com>
2025-12-02 13:26:53 -0800

    refac(can2pwm): Move Enums out of can2pwm
    This will make them easier to use with the classes inside of can2pwm.

--- Currawong/can2pwm_icd.py
+++ Currawong/can2pwm_icd.py
@@ -48,34 +48,38 @@ def set_bits(field: int, value: int, mask: int, shift: int) -> int:
     """Set specific bits in a field to a value."""
     return (field & ~mask) | ((value << shift) & mask)
 
+
+# --- Enums --- #
+
+class deviceType(Enum):
+    SERVO = 0x00
+    CAN2PWM = 0x0A
+
+class inputModes(Enum):
+    STANDBY = 0
+    PWM = 1
+    RPM = 2
+
+class feedbackModes(Enum):
+    NONE = 0
+    RPM = 1
+    PULSE_WIDTH = 2
+    DUTY_CYCLE = 3
+    ANALOG = 4
+
+class messageTypes(Enum):
+    MULTI_COMMAND = 0x00
+    PWM_COMMAND = 0x10
+    HOME_COMMAND = 0x15
+    DISABLE_PACKET = 0x20
+
 class can2pwm():
 
     DEVICE_TYPE = 0x0A
- 
-    # --- Enums --- #
-
-    class deviceType(Enum):
-        SERVO = 0x00
-        CAN2PWM = 0x0A
-
-    class inputModes(Enum):
-        STANDBY = 0
-        PWM = 1
-        RPM = 2
-
-    class feedbackModes(Enum):
-        NONE = 0
-        RPM = 1
-        PULSE_WIDTH = 2
-        DUTY_CYCLE = 3
-        ANALOG = 4
-
-    class messageTypes(Enum):
-        MULTI_COMMAND = 0x00
-        PWM_COMMAND = 0x10
-        HOME_COMMAND = 0x15
-        DISABLE_PACKET = 0x20
-
+        
+        
+# ---- Python-CAN Listener ---- #
+# 
     class PrintListener(Listener):
         def on_message_received(self, msg: Message) -> None:
            """Simple example of a print listener."""
