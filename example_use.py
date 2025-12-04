09927189a54219e24b56bd35bb005055491e5112
Liam Brinston <labrinston@gmail.com>
2025-12-04 15:05:30 -0800

    refac(example_use): Use context manager for bus
    Provides RAII for the bus connection.

--- example_use.py
+++ example_use.py
@@ -25,23 +25,27 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # ---- Bus Setup ---- #
-
-# Setup the bus
 # Linux
-bus = can.Bus(channel = 'can0', interface='socketcan')
-bus = can.Bus(channel = 'can0', interface='socketcan', receive_own_messages=True)
+# bus = can.Bus(channel="can0", interface="socketcan", receive_own_messages=True)
+with can.Bus(channel="can0", interface="socketcan", receive_own_messages=True) as bus:
 
-# Windows
-# bus = can.Bus(channel = '?', interface='pcan') # TODO channel param for windows
+    # Windows
+    # with can.Bus(channel="can0", interface="socketcan", receive_own_messages=True) as bus:
 
-# ---- Find Currawong Devices ---- #
+    # ---- Find Currawong Devices ---- #
 
-dev_tuple = can2pwm.discover_devices(bus)
-print(f"Found: {dev_tuple}")
+    dev_tuple = can2pwm.discover_devices(bus)
+    if dev_tuple:
+        print(f"Found: {dev_tuple}")
+    else:
+        response = input("No devices found on bus. Proceed?")
+        if response.lower() != "y":
+            exit(0)
 
     # ---- (Optional) Set NodeIDs ---- #
 
     if args.dev_ids:
+        logger.info("Setting %s as new device IDs", dev_ids)
         print(f"Setting Device IDs: {args.dev_ids}")
         for serial_num, curr_id in dev_tuple:
             for new_id in args.dev_ids:
@@ -52,70 +56,72 @@ print(f"Found: {dev_tuple}")
                 bus.send(message)
     else:
         print("Skipping -- Set Device IDs (No IDs passed via --dev_ids).")
+    # ---- Enable Telemetry ---- #
 
-# ---- Enable Telemetry ---- #
+    # Enable statusA and statusB
+    telem_packet = can2pwm.TelemetrySettingsPacket(
+        period=100, silence=100, packets=0, statusA=True, statusB=True
+    )
+    # Create a broadcast message - 0xFF
+    telem_message = can2pwm.make_message(telem_packet, 0xFF)
+    print(f"{telem_message}")
+    bus.send(telem_message)
 
-# Enable statusA and statusB
-telem_packet = can2pwm.TelemetrySettingsPacket(period=100, silence=100, 
-                                  packets=0, statusA=True, 
-                                  statusB=True)
-# Create a broadcast message - 0xFF
-telem_message = can2pwm.make_message(telem_packet, 0xFF)
-print(f"{telem_message}")
-bus.send(telem_message)
-       
-# ---- Listener Setup ---- #
-# Turn own receiving own messages
-# bus.receive_own_messages=True    
+    # ---- Listener Setup ---- #
+    # Turn own receiving own messages
+    # bus.receive_own_messages=True
 
-# Create logging config (Note: this only applies to the can2pwm CSVListener)
-log_config = {
-    'PWMCommandPacket': can2pwm.PacketLogConfig(enabled=True, fields=['pwm']),
-    'statusAPacket': can2pwm.PacketLogConfig(enabled=True, fields=['feedback', 'command']),
-    'statusBPacket': can2pwm.PacketLogConfig(enabled=True, fields=['current', 'voltage'])
-}
+    # Create logging config (Note: this only applies to the can2pwm CSVListener)
+    log_config = {
+        "PWMCommandPacket": can2pwm.PacketLogConfig(enabled=True, fields=["pwm"]),
+        "statusAPacket": can2pwm.PacketLogConfig(
+            enabled=True, fields=["feedback", "command", "pwm"]
+        ),
+        "statusBPacket": can2pwm.PacketLogConfig(
+            enabled=True, fields=["current", "voltage"]
+        ),
+    }
 
-# Request listeners from the can2pwm module
-print_listener = can2pwm.PrintListener()
-csv_listener = can2pwm.CSVListener(log_dir="./", log_name="calibration_test",log_config=log_config)
+    # Request listeners from the can2pwm module
+    print_listener = can2pwm.PrintListener()
+    csv_listener = can2pwm.CSVListener(
+        log_dir="./", log_name="calibration_test", log_config=log_config
+    )
 
-# Provide the csv_listener to the notifier
-# Reception and logging of CAN messages will be handled by a separate thread
-notifier = can.Notifier(bus, [csv_listener])
+    # Provide the csv_listener to the notifier
+    # Reception and logging of CAN messages will be handled by a separate thread
+    notifier = can.Notifier(bus, [csv_listener])
 
-# ------------------- #
+    # ------------------- #
 
-# TODO Example transmission code
+    # TODO Example transmission code
 
-time.sleep(2)
-
-# 1. Issue full retraction - 900us
-#    Message - PWMCommand packet 0x07100AXX
-#              OR
-#              MultiCommand packet 0x07000AXX
-#              (would require figuring out how many I need to send)
-pwm_packet = can2pwm.PWMCommandPacket(pwm = 900)
-pwm_msg = can2pwm.make_message(pwm_packet, 0xFF)
-bus.send(pwm_msg)
-
-# 2. Step over range: 900 - 1200us
-# 60us steps should give 2mm
-start = 900
-# stop = 2100
-stop = 1200
-step = 60
-for step in range(start, stop, step):
-    print(f"Stepping: {step}")
-    pwm_packet = can2pwm.PWMCommandPacket(pwm = step)
-    pwm_msg = can2pwm.make_message(pwm_packet, 0xFF)
-    bus.send(pwm_msg)
     time.sleep(2)
 
-# Shut down the notifier thread
-# TODO RAII
-notifier.stop()
-csv_listener.stop()
+    # 1. Issue full retraction - 900us
+    #    Message - PWMCommand packet 0x07100AXX
+    #              OR
+    #              MultiCommand packet 0x07000AXX
+    #              (would require figuring out how many I need to send)
+    pwm_packet = can2pwm.PWMCommandPacket(pwm=900)
+    pwm_msg = can2pwm.make_message(pwm_packet, 0xFF)
+    bus.send(pwm_msg)
 
-# Shut down the bus
-# TODO RAII
-bus.shutdown()
+    # 2. Step over range: 900 - 1200us
+    # 60us steps should give 2mm
+    START = 900
+    # stop = 2100
+    STOP = 1200
+    STEP = 60
+    for step in range(START, STOP, STEP):
+        print(f"Stepping: {step}")
+        pwm_packet = can2pwm.PWMCommandPacket(pwm=step)
+        pwm_msg = can2pwm.make_message(pwm_packet, 0xFF)
+        bus.send(pwm_msg)
+        time.sleep(2)
+
+    # Shut down the notifier thread
+    # Will the bus context manager handle these?
+    # TODO RAII
+    notifier.stop()
+    csv_listener.stop()
